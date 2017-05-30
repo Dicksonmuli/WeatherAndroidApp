3fe04970161d2ebc1741bc5c0770e665ae8c54bb
dicksonmuli <dicksonmully6@gmail.com>
2017-05-30 07:21:53 +0300

    new working space for weather app with MainActivity, Adapter and Weather class files

---
+++ app/src/main/java/com/dicksonmully6gmail/weatherapp/Adapters/DaysListAdapter.java
@@ -0,0 +1,8 @@
+package com.dicksonmully6gmail.weatherapp.Adapters;
+
+/**
+ * Created by dickson on 5/30/17.
+ */
+
+public class DaysListAdapter {
+}
---
+++ app/src/main/java/com/dicksonmully6gmail/weatherapp/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dicksonmully6gmail.weatherapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dicksonmully6gmail.weatherapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
