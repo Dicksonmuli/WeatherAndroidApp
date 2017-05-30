521f65b479ca501ff349e5a5bd2e221dcbf277cd
dicksonmuli <dicksonmully6@gmail.com>
2017-05-30 10:24:13 +0300

    create request using the created url

---
+++ app/src/main/java/com/dicksonmully6gmail/weatherapp/services/WeatherService.java
@@ -0,0 +1,36 @@
+package com.dicksonmully6gmail.weatherapp.services;
+
+import com.dicksonmully6gmail.weatherapp.Constants;
+import com.squareup.picasso.Request;
+
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by dickson on 5/30/17.
+ */
+
+public class WeatherService {
+    public static void searchWeather(String location, Callback callback) {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.API_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.YOUR_QUERY_PARAMETER, location);
+        urlBuilder.addQueryParameter(Constants.API_KEY_QUERY_PARAMETER, Constants.API_KEY);
+
+//        create request using the created url
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+    }
+}
+
+
+    OkHttpClient client = new OkHttpClient.Builder()
+            .build();
+
+    HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.API_BASE_URL).newBuilder();
+urlBuilder.addQueryParameter(Constants.YOUR_QUERY_PARAMETER, <your string parameter here>);
+        urlBuilder.addQueryParameter(Constants.API_KEY_QUERY_PARAMETER, Constants.API_KEY);
+        String url = urlBuilder.build().toString();
--- app/src/main/java/com/dicksonmully6gmail/weatherapp/ui/WeatherActivity.java
+++ app/src/main/java/com/dicksonmully6gmail/weatherapp/ui/WeatherActivity.java
@@ -3,12 +3,19 @@ package com.dicksonmully6gmail.weatherapp.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.dicksonmully6gmail.weatherapp.R;
 
+import java.io.IOException;
+
+import javax.security.auth.callback.Callback;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Response;
 
 /**
  * Created by dickson on 5/30/17.
@@ -29,4 +36,25 @@ public class WeatherActivity extends AppCompatActivity{
 
         getWeather(location);
     }
+    private void getWeather(String location) {
+        final WeatherService weatherService = new WeatherService();
+        weatherService.searchWeather(location, new Callback() {
+
+           @Override
+            public void onFailure(Call call, IOException e) {
+               e.printStackTrace();
+           }
+
+           @Override
+            public void onResponse(Call call, Response response) throws IOException {
+               try {
+                   String jsonData = response.body().string();
+                   Log.v(Tag, jsonData);
+               }catch (IOException e) {
+                   e.printStackTrace();
+               }
+           }
+        });
+    }
 }
+
