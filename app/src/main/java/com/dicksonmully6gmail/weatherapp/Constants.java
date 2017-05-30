599058591e925fcca2b749260d74eaea9fcdabad
dicksonmuli <dicksonmully6@gmail.com>
2017-05-30 07:51:07 +0300

    add Api Key for weather and created String constants for api requirement when the project build

--- app/src/main/java/com/dicksonmully6gmail/weatherapp/Constants.java
+++ app/src/main/java/com/dicksonmully6gmail/weatherapp/Constants.java
@@ -5,9 +5,8 @@ package com.dicksonmully6gmail.weatherapp;
  */
 
 public class Constants {
-    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
-    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
-    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
-    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search?term=food";
-    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+    public static final String API_KEY = BuildConfig.API_KEY;
+    public static final String API_BASE_URL = "www.api.openweathermap.org/data/2.5/weather?q=.com";
+    public static final String YOUR_QUERY_PARAMETER = "location"; //Example: "location"
+    public static final String API_KEY_QUERY_PARAMETER = "appid";
 }
