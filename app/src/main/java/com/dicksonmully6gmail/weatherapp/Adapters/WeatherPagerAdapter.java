405e325fb0301a57e7fa4057c8b6205990c8b58e
dicksonmuli <dicksonmully6@gmail.com>
2017-06-01 10:24:28 +0300

    add weather pager adapter

--- app/src/main/java/com/dicksonmully6gmail/weatherapp/Adapters/WeatherPagerAdapter.java
+++ app/src/main/java/com/dicksonmully6gmail/weatherapp/Adapters/WeatherPagerAdapter.java
@@ -1,8 +1,37 @@
 package com.dicksonmully6gmail.weatherapp.Adapters;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.dicksonmully6gmail.weatherapp.models.Weather;
+
+import java.util.ArrayList;
+
 /**
  * Created by dickson on 6/1/17.
  */
 
-public class WeatherPagerAdapter {
+public class WeatherPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Weather> mWeathers;
+
+    public WeatherPagerAdapter(FragmentManager fm, ArrayList<Weather> weathers) {
+        super(fm);
+        mWeathers = weathers;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return WeathertDetailFragment.newInstance(mWeathers.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mWeathers.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mWeathers.get(position).getMainType();
+    }
 }
---
+++ app/src/main/java/com/dicksonmully6gmail/weatherapp/ui/WeatherDetailFragment.java
@@ -0,0 +1,8 @@
+package com.dicksonmully6gmail.weatherapp.ui;
+
+/**
+ * Created by dickson on 6/1/17.
+ */
+
+public class WeatherDetailFragment {
+}
