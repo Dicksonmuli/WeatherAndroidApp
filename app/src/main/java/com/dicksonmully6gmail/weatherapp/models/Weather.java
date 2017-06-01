d9efb9fa99d52ea4fefe240843cc9c2e3e63a27e
dicksonmuli <dicksonmully6@gmail.com>
2017-06-01 09:16:22 +0300

    parse JSON data and return a list of weather forecast days

--- app/src/main/java/com/dicksonmully6gmail/weatherapp/models/Weather.java
+++ app/src/main/java/com/dicksonmully6gmail/weatherapp/models/Weather.java
@@ -5,25 +5,101 @@ package com.dicksonmully6gmail.weatherapp.models;
  */
 
 
-public class Weather {
-    private String mMain;
-    private String mDescription;
-    private String mIcon;
 
-//    constructor
-    public Weather(String main, String description, String icon) {
-        this.mMain = main;
-        this.mDescription = description;
-        this.mIcon = icon;
+        import org.parceler.Parcel;
+
+        import java.util.ArrayList;
+
+/**
+ * Created by dickson on 5/27/17.
+ */
+
+@Parcel
+public class Restaurant {
+    // fields must be public
+    String mName;
+    String mPhone;
+    String mWebsite;
+    double mRating;
+    String mImageUrl;
+    ArrayList<String> mAddress = new ArrayList<>();
+    double mLatitude;
+    double mLongitude;
+    ArrayList<String> mCategories = new ArrayList<>();
+
+    //    empty constructor needed by the Parceler library
+    public Restaurant() {}
+
+    double time = weatherJSON.getDouble("dt_txt");
+    double temp = weatherJSON
+            .getJSONObject("main").getDouble("temp");
+    double humidity = weatherJSON
+            .getJSONObject("main").getDouble("humidity");
+    double windSpeed = weatherJSON
+            .getJSONObject("wind").getDouble("speed");
+    String mainType = weatherJSON
+            .getJSONObject("weather").getString("main");
+    String description = weatherJSON
+            .getJSONObject("weather").getString("description");
+    String icon = weatherJSON
+            .getJSONObject("weather").getString("icon");
+
+    public Restaurant(String name, String phone, String website,
+                      double rating, String imageUrl, double latitude, double longitude, ArrayList<String> address,
+                      ArrayList<String> categories) {
+        this.mName = name;
+        this.mPhone = phone;
+        this.mWebsite = website;
+        this.mRating = rating;
+        this.mImageUrl = imageUrl;
+        mImageUrl = getLargeImageUrl(imageUrl);
+        this.mAddress = address;
+        this.mLatitude = latitude;
+        this.mLongitude = longitude;
+        this.mCategories = categories;
     }
-//    getter methods
-    public String getMain() {
-        return mMain;
+
+    public String getName() {
+        return mName;
     }
-    public String getDescription() {
-        return mDescription;
+
+    public String getPhone() {
+        return mPhone;
     }
-    public String getIcon() {
-        return mIcon;
+
+    public String getWebsite() {
+        return  mWebsite;
     }
+
+    public double getRating() {
+        return mRating;
+    }
+
+    public String getImageUrl(){
+        return mImageUrl;
+    }
+
+    //refactoring image getter method to retrieve high quality image
+    public String getLargeImageUrl(String imageUrl) {
+        String largeImageUrl = imageUrl.substring(0, imageUrl.length() - 5).concat("o.jpg");
+        return largeImageUrl;
+    }
+
+    public ArrayList<String> getAddress() {
+        return mAddress;
+    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+
+    public ArrayList<String> getCategories() {
+        return mCategories;
+    }
+
+}
 }
--- app/src/main/java/com/dicksonmully6gmail/weatherapp/services/WeatherService.java
+++ app/src/main/java/com/dicksonmully6gmail/weatherapp/services/WeatherService.java
@@ -1,33 +1,81 @@
 package com.dicksonmully6gmail.weatherapp.services;
 
 import com.dicksonmully6gmail.weatherapp.Constants;
+import com.dicksonmully6gmail.weatherapp.models.Weather;
 import com.squareup.picasso.Request;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Response;
 
 
 /**
  * Created by dickson on 5/30/17.
  */
 
-public class WeatherService {
-    public static void searchWeather(String location, Callback callback) {
-        OkHttpClient client = new OkHttpClient.Builder()
-                .build();
-//        HttpUrl class to construct the URL we'll send our request to
-        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.API_BASE_URL).newBuilder();
-        String url = urlBuilder.build().toString() + location + Constants.API_KEY_QUERY_PARAMETER + Constants.API_KEY;
+    public class WeatherService {
+        public static void searchWeather(String location, Callback callback) {
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .build();
+    //        HttpUrl class to construct the URL we'll send our request to
+            HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.API_BASE_URL).newBuilder();
+            String url = urlBuilder.build().toString() + Constants.YOUR_QUERY_PARAMETER + Constants.API_KEY_QUERY_PARAMETER + Constants.API_KEY;
 
-//        create request using the created url
-        okhttp3.Request request = new okhttp3.Request.Builder()
-                .url(url)
-                .build();
+    //        create request using the created url
+            okhttp3.Request request = new okhttp3.Request.Builder()
+                    .url(url)
+                    .build();
 
-        Call call = client.newCall(request);
-        call.enqueue(callback);
+            Call call = client.newCall(request);
+            call.enqueue(callback);
+        }
+    //    parse JSON data and return a list of weather forecast days
+    public ArrayList<Weather> processResults(Response response) {
+        ArrayList<Weather> weathers = new ArrayList<>();
+
+        try {
+            String jsonData = response.body().string();
+            if (response.isSuccessful()) {
+                JSONObject openWeatherMapJSON = new JSONObject(jsonData);
+                JSONArray weatherListJSON = openWeatherMapJSON.getJSONArray("list");
+
+                for (int i = 0; i < weatherListJSON.length(); i++) {
+                    JSONObject weatherJSON = weatherListJSON.getJSONObject(i);
+                  double time = weatherJSON.getDouble("dt_txt");
+                    double temp = weatherJSON
+                            .getJSONObject("main").getDouble("temp");
+                    double humidity = weatherJSON
+                            .getJSONObject("main").getDouble("humidity");
+                    double windSpeed = weatherJSON
+                            .getJSONObject("wind").getDouble("speed");
+                    String mainType = weatherJSON
+                            .getJSONObject("weather").getString("main");
+                    String description = weatherJSON
+                            .getJSONObject("weather").getString("description");
+                    String icon = weatherJSON
+                            .getJSONObject("weather").getString("icon");
+
+
+                }
+            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return weathers;
     }
 }
+
 
