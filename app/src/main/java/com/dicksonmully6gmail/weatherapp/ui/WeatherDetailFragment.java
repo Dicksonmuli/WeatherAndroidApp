9dac5206dac272114318c2956c1344031aa52abe
dicksonmuli <dicksonmully6@gmail.com>
2017-06-01 10:54:44 +0300

    weather object used to set ImageView and TextViews in onCreateView on onCreate view

--- app/src/main/java/com/dicksonmully6gmail/weatherapp/ui/WeatherDetailFragment.java
+++ app/src/main/java/com/dicksonmully6gmail/weatherapp/ui/WeatherDetailFragment.java
@@ -2,93 +2,78 @@ package com.dicksonmully6gmail.weatherapp.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.dicksonmully6gmail.weatherapp.R;
 import com.dicksonmully6gmail.weatherapp.models.Weather;
+import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by dickson on 6/1/17.
  */
 
-public class WeatherDetailFragment extends Fragment implements View.OnClickListener{
-private static final int MAX_WIDTH = 400;
-private static final int MAX_HEIGHT = 300;
-@Bind(R.id.weatherImageView) ImageView mWeatherImageView;//weather icon
-@Bind(R.id.weatherNameTextView) TextView mNameTextView;//weather main type
-@Bind(R.id.descriptionTextView) TextView mDescriptionTextView;//description here
-@Bind(R.id.dateTextView) TextView mDateTextView;//date here
-@Bind(R.id.tempTextView)
-TextView mWebsiteLabel;
+public class WeatherDetailFragment extends Fragment implements View.OnClickListener {
+    private static final int MAX_WIDTH = 400;
+    private static final int MAX_HEIGHT = 300;
+    @Bind(R.id.weatherImageView)
+    ImageView mImageLabel;//weather icon
+    @Bind(R.id.weatherNameTextView)
+    TextView mNameLabel;//weather main type
+    @Bind(R.id.descriptionTextView)
+    TextView mDescriptionLabel;//description here
+    @Bind(R.id.dateTextView)
+    TextView mDateLabel;//date here
+    @Bind(R.id.tempTextView)
+    TextView mTempLabel;
 
 
-//  restaurant object
-private Weather mRestaurant;
+    //  restaurant object
+    private Weather mRestaurant;
 
-public static WeatherDetailFragment newInstance(Weather weather) {
+    public static WeatherDetailFragment newInstance(Weather weather) {
         //wrapping weather with parcels for serialization
-    WeatherDetailFragment weatherDetailFragment = new WeatherDetailFragment();
+        WeatherDetailFragment weatherDetailFragment = new WeatherDetailFragment();
         Bundle args = new Bundle();
         args.putParcelable("weather", Parcels.wrap(weather));
-    weatherDetailFragment.setArguments(args);
+        weatherDetailFragment.setArguments(args);
         return weatherDetailFragment;
-        }
+    }
 
 
-@Override
-public void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
 //        unwrapping restaurant on onCreate
         super.onCreate(savedInstanceState);
         mRestaurant = Parcels.unwrap(getArguments().getParcelable("weather"));
-        }
+    }
 
-//    restaurant object used to set our ImageView and TextViews in onCreateView on onCreate view
-@Override
-public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                         Bundle savedInstanceState) {
+    //    weather object used to set our ImageView and TextViews in onCreateView on onCreate view
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         // Inflating the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_restaurant_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_weather_detail, container, false);
         ButterKnife.bind(this, view);
 
 
         Picasso.with(view.getContext())
-        .load(mRestaurant.getImageUrl())
-        .resize(MAX_WIDTH, MAX_HEIGHT)
-        .centerCrop()
-        .into(mImageLabel);
+                .load(mRestaurant.getIcon())
+                .resize(MAX_WIDTH, MAX_HEIGHT)
+                .centerCrop()
+                .into(mImageLabel);
 
-        mNameLabel.setText(mRestaurant.getName());
-        mCategoriesLabel.setText(android.text.TextUtils.join(", ", mRestaurant.getCategories()));
-        mRatingLabel.setText(Double.toString(mRestaurant.getRating()) + "/5");
-        mPhoneLabel.setText(mRestaurant.getPhone());
-        mAddressLabel.setText(android.text.TextUtils.join(", ", mRestaurant.getAddress()));
+        mNameLabel.setText(mRestaurant.getMainType());
+        mDescriptionLabel.setText(mRestaurant.getDescription());
+        mDateLabel.setText(Double.toString(mRestaurant.getTime()) + "am/pm");
 
-        mWebsiteLabel.setOnClickListener( this);
-        mPhoneLabel.setOnClickListener(this);
-        mAddressLabel.setOnClickListener( this);
         return view;
-        }
-//    implicit intent
-@Override
-public void onClick(View v) {
-        if (v == mWebsiteLabel) {
-        Intent webIntent = new Intent(Intent.ACTION_VIEW,
-        Uri.parse(mRestaurant.getWebsite()));
-        startActivity(webIntent);
-        }
-        if (v == mPhoneLabel) {
-        Intent phoneIntent = new Intent(Intent.ACTION_DIAL,
-        Uri.parse("tel:" + mRestaurant.getPhone()));
-        startActivity(phoneIntent);
-        }
-        if (v == mAddressLabel) {
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW,
-        Uri.parse("geo:" + mRestaurant.getLatitude()
-        + "," + mRestaurant.getLongitude()
-        + "?q=(" + mRestaurant.getName() + ")"));
-        startActivity(mapIntent);
-        }
-        }
+    }
+}
+
